269065df31fa72ae161fcc78c8191a84a2df2a5f
Thisausername99 <39577021+Thisausername99@users.noreply.github.com>
2019-03-16 22:31:25 -0400

    Add files via upload
    split 3 ways into 3 else if statement if do ./wordfreak exit the prog

--- wordfreak.c
+++ wordfreak.c
@@ -24,18 +24,55 @@ bool print_error(int error){
 
 
 
-  int main(int argc, char * argv[], char * envp[]){
-  	int n=0;
-    char temp;
-    Node*head=NULL; //head node
-    char *buffer=(char*)malloc(sizeof(char)); //buffer array
-  	char make_to_word[100]; //array to store the word
+int main(int argc, char * argv[], char * envp[]){
+  int n=0;
+  char temp;
+  Node*head=NULL; //head node
+  char *buffer=(char*)malloc(sizeof(char)); //buffer array
+  char make_to_word[100]; //array to store the word
 
-    if(argc==1){
-      char* file=getenv("WORD_FREAK");
-      if(file==NULL){
-          while(read(STDIN_FILENO,buffer,1)!=0){
-            print_error(errno);
+  if(argc>1){
+    for(int a=1;argv[a]!=NULL;++a){
+      int fd=open(argv[a],O_RDONLY);
+      print_error(errno);
+      
+    while(read(fd,buffer,1)!=0){
+    //printf("%s",buffer);
+      temp=*(buffer);
+      make_to_word[n]=temp;
+      ++n;
+    if(!check_letter(temp)){ //indicate to make a word
+      make_to_word[n-1]='\0';
+      prependNode(&head,make_to_word,1,strlen(make_to_word));
+      n=0;
+      continue;
+        }
+      }
+      close(fd);
+      print_error(errno);
+    }
+  }
+
+  else if(getenv("WORD_FREAK")!=NULL){
+      int fd=open(getenv("WORD_FREAK"),O_RDONLY);
+      print_error(errno);
+      while(read(fd,buffer,1)!=0){
+        temp=*(buffer);
+        make_to_word[n]=temp;
+        ++n;
+      if(!check_letter(temp)){ //indicate to make a word
+        make_to_word[n-1]='\0';
+        prependNode(&head,make_to_word,1,strlen(make_to_word));
+        n=0;
+        continue;
+          }
+        }
+      close(fd);
+      print_error(errno);
+  }
+
+  else if(!isatty(STDIN_FILENO)){
+       while(read(STDIN_FILENO,buffer,1)!=0){// && !isatty(STDIN_FILENO)){
             temp=*(buffer);
             make_to_word[n]=temp;
             ++n;
@@ -44,58 +81,14 @@ bool print_error(int error){
             prependNode(&head,make_to_word,1,strlen(make_to_word)); //add to list  
             n=0;
             continue;
-            }
           }
-        }
+      }
+  }
 
-      else{
-          
-          int fd=open(file,O_RDONLY);
-          print_error(errno);
-          if(fd==-1){//exit if file is not exit
-            exit(EXIT_FAILURE);
-          }
-          else
-          while(read(fd,buffer,1)!=0){
-            temp=*(buffer);
-            make_to_word[n]=temp;
-            ++n;
-          if(!check_letter(temp)){ //indicate to make a word
-            make_to_word[n-1]='\0';
-            prependNode(&head,make_to_word,1,strlen(make_to_word));
-            
-            n=0;
-            continue;
-            }
-          }
-          close(fd);
-          print_error(errno);
-          }
-        }
   else{
-  
-  for(int a=1;argv[a]!=NULL;++a){
-  
-  int fd=open(argv[a],O_RDONLY);
-  print_error(errno);
-
-	while(read(fd,buffer,1)!=0){
-    printf("%s",buffer);
-    temp=*(buffer);
-    make_to_word[n]=temp;
-    ++n;
-    if(!check_letter(temp)){ //indicate to make a word
-      make_to_word[n-1]='\0';
-      prependNode(&head,make_to_word,1,strlen(make_to_word));
-      n=0;
-    continue;
-    }
+    exit(-1);
   }
-  close(fd);
-  print_error(errno);
-  }
-}
-
+  
 print_freq(head);
 free(buffer);
 free_list(head);
