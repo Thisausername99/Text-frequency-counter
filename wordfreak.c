3ca01c18bfcb2330b29812d2b71fc016462e8c9a
nhanfiction <47795547+nhanfiction@users.noreply.github.com>
2019-03-16 23:28:44 -0400

    code format and comments

--- wordfreak.c
+++ wordfreak.c
@@ -11,87 +11,78 @@
 
 extern int errno;
 
-bool print_error(int error){
-      if(error==0){
-        return false;
-      }
-      char* error_msg;
-      sprintf(error_msg,"The error # is %i\n",error);
-      write(STDERR_FILENO,error_msg,strlen(error_msg));
-      exit(error);
-      return true;
+bool print_error(int error) { //writes error message to file descriptor
+  if (error == 0) { //no error
+    return false;
+  }
+  char * error_msg; //string for error message
+  sprintf(error_msg, "The error # is %i\n", error); //format error message
+  write(STDERR_FILENO, error_msg, strlen(error_msg)); //writes to file descriptor
+  exit(error); //exits
+  return true;
 }
 
-
-
-int main(int argc, char * argv[], char * envp[]){
-  int n=0;
-  char temp;
-  Node*head=NULL; //head node
-  char *buffer=(char*)malloc(sizeof(char)); //buffer array
+int main(int argc, char * argv[], char * envp[]) {
+  int n = 0; //counter for index of make_to_word
+  char temp; //temporary character
+  Node * head = NULL; //head node
+  char * buffer = (char * ) malloc(sizeof(char)); //buffer array
   char make_to_word[100]; //array to store the word
 
-  if(argc>1){
-    for(int a=1;argv[a]!=NULL;++a){
-      int fd=open(argv[a],O_RDONLY);
-      print_error(errno);
-      
-    while(read(fd,buffer,1)!=0){
-    //printf("%s",buffer);
-      temp=*(buffer);
-      make_to_word[n]=temp;
-      ++n;
-    if(!check_letter(temp)){ //indicate to make a word
-      make_to_word[n-1]='\0';
-      prependNode(&head,make_to_word,1,strlen(make_to_word));
-      n=0;
-      continue;
+  if (argc > 1) {
+    for (int a = 1; argv[a] != NULL; ++a) { //goes through each file
+      int fd = open(argv[a], O_RDONLY); //opens file to be read
+      print_error(errno); //writes any errors
+      while (read(fd, buffer, 1) != 0) {
+        temp = * (buffer); //temp points to buffer's character
+        make_to_word[n] = temp; //makes word using temp chars
+        ++n; //increment counter
+        if (!check_letter(temp)) { //indicate to make a word
+          make_to_word[n - 1] = '\0'; //sets last character to end of string character
+          prependNode( &head, make_to_word, 1, strlen(make_to_word)); //adds word to list
+          n = 0; //reset index counter
+          continue;
         }
       }
-      close(fd);
-      print_error(errno);
+      close(fd); //close file
+      print_error(errno); //writes any errors
     }
-  }
-
-  else if(getenv("WORD_FREAK")!=NULL){
-      int fd=open(getenv("WORD_FREAK"),O_RDONLY);
-      print_error(errno);
-      while(read(fd,buffer,1)!=0){
-        temp=*(buffer);
-        make_to_word[n]=temp;
-        ++n;
-      if(!check_letter(temp)){ //indicate to make a word
-        make_to_word[n-1]='\0';
-        prependNode(&head,make_to_word,1,strlen(make_to_word));
-        n=0;
+  } 
+  else if (getenv("WORD_FREAK") != NULL) { //if environment variable used
+    int fd = open(getenv("WORD_FREAK"), O_RDONLY); //opens file to be read
+    print_error(errno); //writes any errors
+    while (read(fd, buffer, 1) != 0) {
+      temp = * (buffer); //temp points to buffer's character
+      make_to_word[n] = temp; //makes word using temp chars
+      ++n; //increment counter
+      if (!check_letter(temp)) { //indicate to make a word
+        make_to_word[n - 1] = '\0'; //sets last character to end of string character
+        prependNode( & head, make_to_word, 1, strlen(make_to_word)); //adds word to list
+        n = 0; //resets index counter
         continue;
-          }
-        }
-      close(fd);
-      print_error(errno);
-  }
-
-  else if(!isatty(STDIN_FILENO)){
-       while(read(STDIN_FILENO,buffer,1)!=0){// && !isatty(STDIN_FILENO)){
-            temp=*(buffer);
-            make_to_word[n]=temp;
-            ++n;
-          if(!check_letter(temp)){ //indicate to make a word
-            make_to_word[n-1]='\0';
-            prependNode(&head,make_to_word,1,strlen(make_to_word)); //add to list  
-            n=0;
-            continue;
-          }
       }
-  }
-
-  else{
+    }
+    close(fd); //close file
+    print_error(errno); //writes any errors
+  } 
+  else if (!isatty(STDIN_FILENO)) {
+    while (read(STDIN_FILENO, buffer, 1) != 0) { //while not end of file
+      temp = * (buffer); //temp points to buffer's character
+      make_to_word[n] = temp; //makes word using temp chars
+      ++n; //increment counter
+      if (!check_letter(temp)) { //indicate to make a word
+        make_to_word[n - 1] = '\0'; //sets last character to end of string character
+        prependNode( &head, make_to_word, 1, strlen(make_to_word)); //adds word to list  
+        n = 0; //resets index counter
+        continue;
+      }
+    }
+  } 
+  else {
     exit(-1);
   }
-  
-print_freq(head);
-free(buffer);
-free_list(head);
-
-return 0; 
+  print_freq(head); //format every node and writes them
+  free(buffer); //frees buffer
+  free_list(head); //frees list of words
+  return 0;
 }
