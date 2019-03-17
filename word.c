20d17f813a2f2deacb9f613a8f3a3f8e1c9d6f09
nhanfiction <47795547+nhanfiction@users.noreply.github.com>
2019-03-16 22:39:01 -0400

    code format and comments

--- word.c
+++ word.c
@@ -7,32 +7,27 @@
 #include <stdlib.h>
 #include <assert.h>
 
-//extern int max_len;
 
-
-char *copy_word (char *word) { //copy the word by value
-  assert(word != NULL);
-  int len = strlen(word);
-  char *copy = (char *)(malloc(len+1));
-  assert(copy != NULL);
-  strcpy(copy, word);
+char * copy_word(char * word) { //copy the word
+  assert(word != NULL); //word not null
+  int len = strlen(word); //length of the word
+  char * copy = (char * )(malloc(len + 1)); //allocates space for the word
+  assert(copy != NULL); //copy not null
+  strcpy(copy, word); //copy and word 
   return copy;
 }
 
-
-bool check_letter(char c){ // check if it the letter
-  if( (c>='a' && c<='z') || (c>='A' && c<='Z') || c==39)//check if char is letter or aphostrope
-        return true;
-    else
-      return false;
+bool check_letter(char c) { //check if it the letter
+  if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == 39) //check if char is letter or apostrophe
+    return true;
+  else
+    return false;
 
 }
 
-int freq_len(int freq){
-  if(freq == 0){
+int freq_len(int freq) { //number of digits in number
+  if (freq == 0) {
     return 0;
   }
-  return 1 + freq_len(freq/10);
+  return 1 + freq_len(freq / 10); //recurses and adds one
 }
-
-
